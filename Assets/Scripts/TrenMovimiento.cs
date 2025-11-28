9b791f6be2e11a2bdf69a25d640dd339170335e4
marrazki <martin_leoz@euneiz.com>
2025-11-29 00:35:37 +0100

    LERP

--- Assets/Scripts/TrenMovimiento.cs
+++ Assets/Scripts/TrenMovimiento.cs
@@ -1,61 +1,69 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrenMovimiento : MonoBehaviour
 {
-    [SerializeField] public float velocidad;
+    [SerializeField] public float velocidad = 5f;
     public GameManager gameManager;
     public GameObject resetPoint;
     public GameObject actionPoint;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float velocidadVertical = 5f; // Velocidad del Lerp vertical
+
+    private Vector3 targetPosition;
+
     void Start()
     {
-        
+        targetPosition = transform.position; // iniciar posición objetivo
     }
 
-    // Update is called once per frame
     void Update()
     {
-        this.transform.position = transform.position + new Vector3(velocidad,0,0) * Time.deltaTime;//Movimiento eje x
+        // Movimiento horizontal constante
+        transform.position += new Vector3(velocidad, 0, 0) * Time.deltaTime;
+
+        // Movimiento vertical suave hacia la posición objetivo
+        transform.position = Vector3.Lerp(
+            transform.position,
+            new Vector3(transform.position.x, targetPosition.y, transform.position.z),
+            Time.deltaTime * velocidadVertical
+        );
     }
+
     void IrPorArriba()
     {
-        this.transform.position = this.transform.position + new Vector3(0, 3, 0);
+        targetPosition = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
     }
+
     void IrPorAbajo()
     {
-        this.transform.position = this.transform.position + new Vector3(0, -3, 0);
+        targetPosition = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z);
     }
+
     void IrPorElMedio()
     {
-        transform.position = new Vector3(
-        transform.position.x,
-        0,
-        transform.position.z
-    );
+        targetPosition = new Vector3(transform.position.x, 0, transform.position.z);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("ResetPoint"))
         {
             Debug.Log("RESET");
-            gameManager.decision = 0; //RESETEO DE LA DECISION
-            resetPoint.transform.position += new Vector3(40, 0, 0);//MOVER EL RESETPOINT
+            gameManager.decision = 0;
+            resetPoint.transform.position += new Vector3(40, 0, 0);
             IrPorElMedio();
         }
+
         if (collision.CompareTag("ActionPoint"))
         {
             Debug.Log("ACCION");
-            //ACCION
-            actionPoint.transform.position += new Vector3(40, 0, 0);//MOVER EL ACTIONPOINT
+            actionPoint.transform.position += new Vector3(40, 0, 0);
+
             if (gameManager.decision == 1)
-            {
                 IrPorArriba();
-            }
+
             if (gameManager.decision == 2)
-            {
                 IrPorAbajo();
-            }
         }
     }
 }
